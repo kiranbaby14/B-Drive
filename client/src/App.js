41dced21fffe2611010d13de624b63c0ee069be2
kiranbaby14 <kiranbaby256@gmail.com>
2023-07-29 01:07:37 +0100

    update hardhat version

--- client/src/App.js
+++ client/src/App.js
@@ -34,7 +34,7 @@ function App() {
         const signer = provider.getSigner();
         const address = await signer.getAddress();
         setAccount(address);
-        let contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+        let contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"; // replace with the deployed contract address
 
         const contract = new ethers.Contract(
           contractAddress,
